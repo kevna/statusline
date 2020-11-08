e8c8615c907cf2a1a8f452edc94396dc38fafc28
kevna <lakevna@gmail.com>
2020-11-08 20:36:36 +0000

    added setup.py to install package via pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name = "statusline",
+        author = "Aaron Moore",
+        description = "display directory and repository stats for statusline",
+        packages = ["statusline"],
+        entry_points = {
+            "console_scripts": [
+                "statusline = statusline.__main__:main"
+            ]
+        })
