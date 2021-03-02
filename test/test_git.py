2616d1ed4527a55f52cb065a5c77fd4cd34ce080
kevna <lakevna@gmail.com>
2021-03-02 19:03:49 +0000

    pep8 quotes for test cases

--- test/test_git.py
+++ test/test_git.py
@@ -11,141 +11,141 @@ def instance():
     return Git()
 
 
-@pytest.mark.parametrize("cmd, mock, expected_return, expected_call", (
+@pytest.mark.parametrize('cmd, mock, expected_return, expected_call', (
     (
-        ["stash", "list", "--porcelain"],
-        SimpleNamespace(stdout=b"stash@{0}\nstash@{1}\n"),
-        "stash@{0}\nstash@{1}\n",
-        call(["git", "stash", "list", "--porcelain"], check=True, capture_output=True)
+        ['stash', 'list', '--porcelain'],
+        SimpleNamespace(stdout=b'stash@{0}\nstash@{1}\n'),
+        'stash@{0}\nstash@{1}\n',
+        call(['git', 'stash', 'list', '--porcelain'], check=True, capture_output=True)
     ),
 ))
 def test__run_command(cmd, mock, expected_return, expected_call, instance):
-    with patch("statusline.git.run", return_value=mock) as mock:
+    with patch('statusline.git.run', return_value=mock) as mock:
         actual = instance._run_command(cmd)
         assert actual == expected_return
         assert mock.call_args == expected_call
 
 
-@pytest.mark.parametrize("cmd, mock, expected", (
-    (["stash", "list"], "", 0),
-    (["stash", "list"], "stash@{0}\nstash@{1}\n", 2),
+@pytest.mark.parametrize('cmd, mock, expected', (
+    (['stash', 'list'], '', 0),
+    (['stash', 'list'], 'stash@{0}\nstash@{1}\n', 2),
 ))
 def test__count(cmd, mock, expected, instance):
-    with patch("statusline.git.Git._run_command", return_value=mock) as mock:
+    with patch('statusline.git.Git._run_command', return_value=mock) as mock:
         actual = instance._count(cmd)
         assert actual == expected
         assert mock.call_args == call(cmd)
 
 
-@patch("statusline.git.Git._run_command", return_value="~/.local/chezmoi\n")
+@patch('statusline.git.Git._run_command', return_value='~/.local/chezmoi\n')
 def test_root_dir_cached(mock, instance):
-    instance._root = "/path/"
-    assert instance.root_dir == "/path/"
+    instance._root = '/path/'
+    assert instance.root_dir == '/path/'
     assert not mock.called
 
 
-@patch("statusline.git.Git._run_command", return_value="~/.local/chezmoi\n")
+@patch('statusline.git.Git._run_command', return_value='~/.local/chezmoi\n')
 def test_root_dir_calculate(mock, instance):
-    assert instance.root_dir == "~/.local/chezmoi"
-    assert mock.call_args == call(["rev-parse", "--show-toplevel"])
+    assert instance.root_dir == '~/.local/chezmoi'
+    assert mock.call_args == call(['rev-parse', '--show-toplevel'])
 
 
-@patch("statusline.git.Git._run_command", return_value="  master  ")
+@patch('statusline.git.Git._run_command', return_value='  master  ')
 def test_branch(mock, instance):
-    assert instance.branch == "master"
+    assert instance.branch == 'master'
     assert mock.call_args == call(
-        ["rev-parse", "--symbolic-full-name", "--abbrev-ref", "HEAD"]
+        ['rev-parse', '--symbolic-full-name', '--abbrev-ref', 'HEAD']
     )
 
 
-@patch("statusline.git.path.getmtime", return_value=1604363715.999)
+@patch('statusline.git.path.getmtime', return_value=1604363715.999)
 def test_last_fetch(mock, instance):
-    instance._root = "root"
+    instance._root = 'root'
     assert instance.last_fetch == 1604363715
-    assert mock.call_args == call("root/.git/FETCH_HEAD")
+    assert mock.call_args == call('root/.git/FETCH_HEAD')
 
 
-@pytest.mark.parametrize("exists, root, expected", (
+@pytest.mark.parametrize('exists, root, expected', (
     (True, None, True),
-    (False, "root", True),
-    (False, "", False),
+    (False, 'root', True),
+    (False, '', False),
     (False, None, False),
 ))
 def test_has_vcs(exists, root, expected, instance):
-    with patch("statusline.git.path.exists", return_value=exists) as mock_exists, \
-        patch("statusline.git.Git.root_dir", new_callable=PropertyMock, return_value=root):
+    with patch('statusline.git.path.exists', return_value=exists) as mock_exists, \
+        patch('statusline.git.Git.root_dir', new_callable=PropertyMock, return_value=root):
         actual = instance.has_vcs()
         assert actual == expected
-        mock_exists.called_once_with(".git")
+        mock_exists.called_once_with('.git')
 
 
-@pytest.mark.parametrize("porcelain, expected", (
+@pytest.mark.parametrize('porcelain, expected', (
     ([0, 0], (0, 0)),
     ([5, 10], (5, 10)),
 ))
 def test_ahead_behind(porcelain, expected, instance):
-    with patch("statusline.git.Git._count", side_effect=porcelain) as mock:
+    with patch('statusline.git.Git._count', side_effect=porcelain) as mock:
         actual = instance.ahead_behind()
         assert actual == expected
         assert mock.call_args_list == [
-            call(["rev-list", "@{u}..HEAD"]),
-            call(["rev-list", "HEAD..@{u}"]),
+            call(['rev-list', '@{u}..HEAD']),
+            call(['rev-list', 'HEAD..@{u}']),
         ]
 
 
-@pytest.mark.parametrize("porcelain, expected", (
-    ("", (0, 0, 0)),
-    ("?? untrack.ed\nM  stag.ed\n M unstag.ed", (1, 1, 1)),
+@pytest.mark.parametrize('porcelain, expected', (
+    ('', (0, 0, 0)),
+    ('?? untrack.ed\nM  stag.ed\n M unstag.ed', (1, 1, 1)),
 ))
 def test_status(porcelain, expected, instance):
-    with patch("statusline.git.Git._run_command", return_value=porcelain) as mock:
+    with patch('statusline.git.Git._run_command', return_value=porcelain) as mock:
         actual = instance.status()
         assert actual == expected
-        assert mock.call_args == call(["status", "--porcelain"])
+        assert mock.call_args == call(['status', '--porcelain'])
 
 
-@patch("statusline.git.Git._count", return_value=1)
+@patch('statusline.git.Git._count', return_value=1)
 def test_stashes(mock, instance):
     actual = instance.stashes()
     assert actual == 1
-    assert mock.call_args == call(["stash", "list"])
+    assert mock.call_args == call(['stash', 'list'])
 
 
-@pytest.mark.parametrize("branch, ab, status, stashes, expected", (
-    ("master", AheadBehind(0, 0), Status(0, 0, 0), 0, "\uE0A0\033[0mmaster"),
+@pytest.mark.parametrize('branch, ab, status, stashes, expected', (
+    ('master', AheadBehind(0, 0), Status(0, 0, 0), 0, '\uE0A0\033[0mmaster'),
     (
-        "master",
+        'master',
         AheadBehind(1, 0),
         Status(3, 2, 0),
         0,
-        "\uE0A0\033[0mmaster↑1(\033[32m3\033[31m2\033[0m)",
+        '\uE0A0\033[0mmaster↑1(\033[32m3\033[31m2\033[0m)',
     ),
     (
-        "master",
+        'master',
         AheadBehind(0, 1),
         Status(0, 0, 5),
         0,
-        "\uE0A0\033[0mmaster↓1(\033[90m5\033[0m)",
+        '\uE0A0\033[0mmaster↓1(\033[90m5\033[0m)',
     ),
     (
-        "master",
+        'master',
         AheadBehind(3, 2),
         Status(0, 0, 0),
         1,
-        "\uE0A0\033[0mmaster\033[30;101m↕5\033[0m{1}",
+        '\uE0A0\033[0mmaster\033[30;101m↕5\033[0m{1}',
     ),
 ))
 # TODO should we mock more of the ansi calls here?
-@patch("statusline.git.rgb.rgb256", return_value="")
+@patch('statusline.git.rgb.rgb256', return_value='')
 def test_short_stats(mock, branch, ab, status, stashes, expected, instance):
     with patch(
-        "statusline.git.Git.branch", new_callable=PropertyMock, return_value=branch
+        'statusline.git.Git.branch', new_callable=PropertyMock, return_value=branch
     ), patch(
-        "statusline.git.Git.ahead_behind", return_value=ab
+        'statusline.git.Git.ahead_behind', return_value=ab
     ), patch(
-        "statusline.git.Git.status", return_value=status
+        'statusline.git.Git.status', return_value=status
     ), patch(
-        "statusline.git.Git.stashes", return_value=stashes
+        'statusline.git.Git.stashes', return_value=stashes
     ):
         actual = instance.short_stats()
         assert actual == expected
--- test/test_status.py
+++ test/test_status.py
@@ -13,52 +13,52 @@ def instance():
     return result
 
 
-@pytest.mark.parametrize("name, expected", (
-    ("~", "~"),
-    ("~root", "~r"),
-    ("private_dot_config", "p"),
-    ("._shares", "._"),
+@pytest.mark.parametrize('name, expected', (
+    ('~', '~'),
+    ('~root', '~r'),
+    ('private_dot_config', 'p'),
+    ('._shares', '._'),
 ))
 def test__minify_dir(name, expected, instance):
     actual = instance._minify_dir(name)
     assert actual == expected
 
 
-@pytest.mark.parametrize("path, expected", (
-    ("~", "~"),
-    ("/etc/X11/xorg.conf.d", "/e/X/xorg.conf.d"),
-    ("~/.local/share/chezmoi/private_dot_config/i3", "~/.l/s/c/p/i3"),
+@pytest.mark.parametrize('path, expected', (
+    ('~', '~'),
+    ('/etc/X11/xorg.conf.d', '/e/X/xorg.conf.d'),
+    ('~/.local/share/chezmoi/private_dot_config/i3', '~/.l/s/c/p/i3'),
 ))
-@patch("statusline.status.DirectoryMinify.hi", side_effect=lambda x: x)
+@patch('statusline.status.DirectoryMinify.hi', side_effect=lambda x: x)
 def test_minify_path(mock, path, expected, instance):
     actual = instance.minify_path(path)
     assert actual == expected
 
 
-@pytest.mark.parametrize("path, expected", (
-    ("/home/kevna", "~"),
-    ("/home/kevna/.config", "~/.config"),
+@pytest.mark.parametrize('path, expected', (
+    ('/home/kevna', '~'),
+    ('/home/kevna/.config', '~/.config'),
 ))
-@patch("statusline.status.DirectoryMinify.hi", side_effect=lambda x: x)
+@patch('statusline.status.DirectoryMinify.hi', side_effect=lambda x: x)
 def test_minify_path_home(mock, path, expected, instance):
-    actual = instance.minify_path(path, home="/home/kevna")
+    actual = instance.minify_path(path, home='/home/kevna')
     assert actual == expected
 
 
-@patch("statusline.status.DirectoryMinify.minify_path", side_effect=["~/.l/s/chezmoi", "/p/i3"])
+@patch('statusline.status.DirectoryMinify.minify_path', side_effect=['~/.l/s/chezmoi', '/p/i3'])
 def test__apply_vcs(mock_minify, instance):
-    instance.VCS.root_dir = "/home/kevna/.local/share/chezmoi"
-    instance.VCS.short_stats.return_value = "\uE0A0master"
-    actual = instance._apply_vcs("/home/kevna/.local/share/chezmoi/private_dot_config/i3")
-    assert actual == "~/.l/s/chezmoi\uE0A0master/p/i3"
+    instance.VCS.root_dir = '/home/kevna/.local/share/chezmoi'
+    instance.VCS.short_stats.return_value = '\uE0A0master'
+    actual = instance._apply_vcs('/home/kevna/.local/share/chezmoi/private_dot_config/i3')
+    assert actual == '~/.l/s/chezmoi\uE0A0master/p/i3'
     mock_minify.assert_has_calls([
-        call("/home/kevna/.local/share/chezmoi"),
-        call("/private_dot_config/i3")
+        call('/home/kevna/.local/share/chezmoi'),
+        call('/private_dot_config/i3')
     ])
 
 
-@patch("statusline.status.os.getcwd", return_value="/home/kevna/.local/share/chezmoi")
-@patch("statusline.status.DirectoryMinify._apply_vcs", return_value="~/.l/s/chezmoi")
+@patch('statusline.status.os.getcwd', return_value='/home/kevna/.local/share/chezmoi')
+@patch('statusline.status.DirectoryMinify._apply_vcs', return_value='~/.l/s/chezmoi')
 def test_get_statusline_git(mock_minify, mock_cwd, instance):
     instance.VCS.has_vcs.return_value = True
     actual = instance.get_statusline()
@@ -67,8 +67,8 @@ def test_get_statusline_git(mock_minify, mock_cwd, instance):
     mock_minify.assert_called_once_with(mock_cwd.return_value)
 
 
-@patch("statusline.status.os.getcwd", return_value="/home/kevna/.local/share/chezmoi")
-@patch("statusline.status.DirectoryMinify.minify_path", return_value="~/.l/s/chezmoi")
+@patch('statusline.status.os.getcwd', return_value='/home/kevna/.local/share/chezmoi')
+@patch('statusline.status.DirectoryMinify.minify_path', return_value='~/.l/s/chezmoi')
 def test_get_statusline_nogit(mock_minify, mock_cwd, instance):
     instance.VCS.has_vcs.return_value = False
     actual = instance.get_statusline()
