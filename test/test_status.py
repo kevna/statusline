30e0e35f8e46326c5939e8d86886d8696ca0a2d1
kevna <lakevna@gmail.com>
2021-03-23 19:06:11 +0000

    unittests for status path kwarg

--- test/test_status.py
+++ test/test_status.py
@@ -8,9 +8,11 @@ from statusline.git import Git
 
 @pytest.fixture()
 def instance():
-    result = DirectoryMinify()
-    result.VCS = MagicMock(spec=Git)
-    return result
+    return DirectoryMinify()
+
+@pytest.fixture()
+def mock_vcs():
+    return MagicMock(spec=Git)
 
 
 @pytest.mark.parametrize('name, expected', (
@@ -46,32 +48,38 @@ def test_minify_path_home(mock, path, expected, instance):
 
 
 @patch('statusline.status.DirectoryMinify.minify_path', side_effect=['~/.l/s/chezmoi', '/p/i3'])
-def test__apply_vcs(mock_minify, instance):
-    instance.VCS.root_dir = '/home/kevna/.local/share/chezmoi'
-    instance.VCS.short_stats.return_value = '\uE0A0master'
-    actual = instance._apply_vcs('/home/kevna/.local/share/chezmoi/private_dot_config/i3')
+def test__apply_vcs(mock_minify, instance, mock_vcs):
+    mock_vcs.root_dir = '/home/kevna/.local/share/chezmoi'
+    mock_vcs.short_stats.return_value = '\uE0A0master'
+    actual = instance._apply_vcs('/home/kevna/.local/share/chezmoi/private_dot_config/i3', mock_vcs)
     assert actual == '~/.l/s/chezmoi\uE0A0master/p/i3'
-    mock_minify.assert_has_calls([
+    assert mock_minify.call_args_list == [
         call('/home/kevna/.local/share/chezmoi'),
         call('/private_dot_config/i3')
-    ])
+    ]
 
 
-@patch('statusline.status.os.getcwd', return_value='/home/kevna/.local/share/chezmoi')
 @patch('statusline.status.DirectoryMinify._apply_vcs', return_value='~/.l/s/chezmoi')
-def test_get_statusline_git(mock_minify, mock_cwd, instance):
-    instance.VCS.__bool__.return_value = True
-    actual = instance.get_statusline()
+@patch('statusline.status.Git')
+def test_get_statusline_git(mock_vcs, mock_minify, instance):
+    path = '/home/kevna/.local/share/chezmoi'
+    mock_bool = mock_vcs.return_value.__bool__
+    mock_bool.return_value = True
+    instance.VCS = mock_vcs
+    actual = instance.get_statusline(path=path)
     assert actual == mock_minify.return_value
-    assert instance.VCS.__bool__.called
-    mock_minify.assert_called_once_with(mock_cwd.return_value)
+    assert mock_bool.called
+    assert mock_minify.call_args == call(path, mock_vcs.return_value)
 
 
-@patch('statusline.status.os.getcwd', return_value='/home/kevna/.local/share/chezmoi')
 @patch('statusline.status.DirectoryMinify.minify_path', return_value='~/.l/s/chezmoi')
-def test_get_statusline_nogit(mock_minify, mock_cwd, instance):
-    instance.VCS.__bool__.return_value = False
-    actual = instance.get_statusline()
+@patch('statusline.status.Git')
+def test_get_statusline_nogit(mock_vcs, mock_minify, instance):
+    path = '/home/kevna/.local/share/chezmoi'
+    mock_bool = mock_vcs.return_value.__bool__
+    mock_bool.return_value = False
+    instance.VCS = mock_vcs
+    actual = instance.get_statusline(path=path)
     assert actual == mock_minify.return_value
-    assert instance.VCS.__bool__.called
-    mock_minify.assert_called_once_with(mock_cwd.return_value)
+    assert mock_bool.called
+    assert mock_minify.call_args == call(path)
