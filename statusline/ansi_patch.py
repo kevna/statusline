93f09a5a93e4271c3ddb60bee226d48198ea5dcd
kevna <lakevna@gmail.com>
2021-03-25 00:01:35 +0000

    linting changes to monkey patch

--- statusline/ansi_patch.py
+++ statusline/ansi_patch.py
@@ -3,6 +3,7 @@ from ansi.sequence import sequence
 
 
 def patch(self):
+    """Monkeypatch for Graphic.__str__ with non-printing character escapes added."""
     return f'\001{self.sequence}\002'
 
 
@@ -10,5 +11,8 @@ Graphic.__str__ = patch
 
 
 def colour256(colour):
+    """Generate a 256 colour escape.
+    This method takes a 256 colour code rather than taking 'true colour' and estimating it.
+    """
     seq = sequence('m', fields=3)(38, 5, colour)
     return f'\001{seq}\002'
